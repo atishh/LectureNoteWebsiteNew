7dafcb6387a5cda0f288bfcd94c3c916de6d82a7
atish <7705506+atishh@users.noreply.github.com>
2017-01-31 03:23:11 +0530

    Adding javascript

---
+++ js/main.js
@@ -0,0 +1,30 @@
+function displayVideoPdf(divId, youVideo, pdfFile) {
+
+	var divElement = document.getElementById(divId); 
+	if(divElement.style.display == "none") {
+
+		var pdfFileFullPath = '/home/atish/Desktop/lecture videos/';
+		pdfFileFullPath += pdfFile;
+		var statement = '<div class="home-columns clearfix">' ;
+		statement += '<div class="one-half"> <iframe class="iframe-size" src="http://www.youtube.com/embed/';
+		statement += youVideo;
+		statement += '" width="400" height="200" frameborder="0" allowfullscreen="allowfullscreen"></iframe></div>';
+		statement += '<div class="one-half last"> <iframe class="iframe-size" src="';
+		statement += pdfFileFullPath;
+		statement += '" style="width:400px; height:200px;" frameborder="0" allowfullscreen=""></iframe> <nav class="site-nav site-header-nav departments-nav clearfix"> <ul> <li><a href="';
+		statement += pdfFileFullPath;
+		statement += '" download="';
+		statement += pdfFile;
+		statement += '" class = "btn-element" >Download</a></li> <li><a href="';
+		statement += pdfFileFullPath;
+		statement += '" class = "btn-element" >View</a></li> </ul></nav></div></div>';
+	
+		divElement.innerHTML = statement;
+		divElement.style.display = "block"; 
+	}
+	else {
+		divElement.innerHTML = "";
+		divElement.style.display = "none"; 
+	}
+
+}
